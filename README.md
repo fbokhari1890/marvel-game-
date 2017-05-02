df73248daf557afb2f057ebdb790e0709926e0b5
Fahim Bokhari <fahimbokhari@Fahims-MacBook-Air.local>
2017-05-02 18:42:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# PROJECT 2: SUPERHERO APPLICATION
+
+Growing up I was always mesmerized by the superheroes and their ability to be provided with supernatural powers.  The acts of altruism speaks to the human spirit and our intate drive to seek goodness.  The reminscing back to the  nostalgia running through comicbook stores as a child inspired me to tap into the Marvel Universe to create an application that list all the superheroes that became staples of my childhood.  
+
+
+
+![comic](./assets/comic.jpg)
+
+### TECHNOLOGIES USED
+
+After a rigourous and thought provoking guide to building a collective CRUD based "quotes app"; I aim to use an express server to create a database with a collection of superheroes.  The application will combine the use of SQL through POSTGRESS and 'fetch' some data from the open-source Marvel API.  The project will incorporate both the element of client side and server-side.  I will be using Effective Javascript (.ejs) and partials to create rich content to appear on your local computer screen.
+
+Below is the link for the Marvel API (in case you were curious):
+
+https://developer.marvel.com/
+
+### USER EXPRIENCE 
+
+I want to create an application that has the ability for the USER to view some of my favorite superheroes.  The USER should be able to create a new superhero or edit the existing superhero. There will be a login if I reach post MVP that allows USERS to save their favorite users.  THe application will have all the Create Read Update Delete (CRUD) features.  
+
+
+### CHALLENGES
+
+The SQL query will probably be able to hard to approach; to try to create call a query. I think the login will be diffcult to reach and the search for the superhero. I think I will learn the most from the errors I will encounter.
+
+
+### WIREFRAMES
+
+
+
+
+![wireframes](./assets/wireframe_comic1.jpg)
+![wireframes](./assets/wireframe_comic2.jpg)
+![wireframes](./assets/wireframe_comic3.jpg)
+![wireframes](./assets/wireframe_comic4.jpg)
+![wireframes](./assets/wireframe_comic5.jpg)
+![wireframes](./assets/wireframe_comic6.jpg)
+
+
+
+
+
+
+
+
+
---
+++ db/hero.js
@@ -0,0 +1,88 @@
+const hero = [
+  {
+    name: 'Spiderman',
+    realName: 'Peter Parker',
+    location: 'Queens, NY',
+    description: 'Bitten by a radioactive spider, high school student Peter Parker gained the speed, strength and powers of a spider Adopting the name Spider-Man, Peter hoped to start a career using his new abilities Taught that with great power comes great responsibility, Spidey has vowed to use his powers to help people',
+  },
+  {
+    name: 'Daredevil',
+    realName: 'Matt Murdock',
+    location: 'Hells Kitchen, NY',
+    description: 'A radioactive cargo that rendered Matt blind while enhancing his remaining senses Matt mastered his heightened senses and became a formidable fighter',
+   },
+  {
+    name: 'Punisher',
+    realName: 'Frank Castle',
+    location: 'Queens, NY',
+    description: 'Wages a one-man war on the mob and all criminals in general by using all manner of conventional war weaponry',
+  },
+  {
+    name: 'Deadpool',
+    realName: 'Wade Wilson',
+    location: 'Canada',
+    description: 'A disfigured and mentally unstable mercenary with the superhuman ability of an accelerated healing factor and physical prowess The character is known as the "Merc with a Mouth" because of his talkative nature and tendency to break the fourth wall, which is used by writers for humorous effect and running gags',
+  },
+  {
+    name: 'Wolverine',
+    realName: 'James Logan Howlett',
+    location: 'Canada',
+    description: 'Born with super-human senses and the power to heal from almost any wound, Wolverine was captured by a secret Canadian organization and given an unbreakable skeleton and claws Treated like an animal, it took years for him to control himself',
+  },
+  {
+    name: 'Hulk',
+    realName:'Bruce Banner'
+    location: 'Dayton, OH',
+    description: 'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr Bruce Banner was transformed into the incredibly powerful creature called the Hulk An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets',
+  },
+   {
+    name: 'Thor',
+    realName: 'Thor Odinson',
+    location: 'Asgard',
+    description: 'As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir',
+  },
+   {
+    name: 'Jean Grey',
+    realName: 'Jean Grey-Summers',
+    location: 'Annandale-On-Hudson, NY',
+    description: 'possessed telepathic powers enabling her to read minds, project her thoughts into the minds of others, initiate astral travel, and mentally stun opponents with pure psionic force, among other talents She also possessed telekinesis, allowing her to levitate and manipulate objects and others, generate force fields, fly, and stimulate heat molecules to generate concussive blasts',
+  },
+   {
+    name: 'Captain America',
+    realName: 'Steve Rogers',
+    location: 'Brooklyn, NY',
+    description: 'Vowing to serve his country any way he could, young Steve Rogers took the super soldier serum to become Americas one-man army; the living, breathing symbol of freedom and liberty',
+  },
+   {
+    name: 'Iron-Man',
+    realName: 'Tony Stark',
+    location: 'Long Island, NY',
+    description: 'Billionaire party boy wounded, captured and forced to build a weapon by his enemies; created an advanced suit of armor to save his life and escape captivity',
+  },
+   {
+    name: 'Black Panther',
+    realName: 'TChalla',
+    location: 'Wakanda',
+    description: 'a royal descendent of a warrior race is also a master of armed combat, able to use a variety of weapons but prefers unarmed combat',
+  },
+   {
+    name: 'The Human Torch',
+    realName: 'Johnny Storm',
+    location: 'Long Island, NY',
+    description: ' gained his powers on a spacecraft bombarded by cosmic rays He can engulf his entire body in flames, fly, absorb fire harmlessly into his own body, and control any nearby fire by sheer force of will',
+  },
+   {
+    name: 'Magneto',
+    realName: 'Max Eisenhardt',
+    location: 'Germany',
+    description: 'has the ability to generate and control magnetic fields Magneto regards mutants as evolutionarily superior to humans and rejects the possibility of peaceful human-mutant coexistence; he aims to conquer the world to enable mutants',
+  },
+    {
+    name: 'Luke Cage',
+    realName: 'Carl Lucas',
+    location: 'Harlem, NY',
+    description: 'gains the powers of superhuman strength and unbreakable skin after being subjected involuntarily to an experimental procedure',
+  },
+];
+
+module.exports = hero;
